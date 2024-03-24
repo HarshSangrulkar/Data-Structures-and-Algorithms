0958eb0f964b1e97dbf3f984a2d46e9e1dffddfc
Harsh Sangrulkar <harshsangrulkar@gmail.com>
2024-03-24 16:24:16 +0530

    check palindrome in Linked List

--- SDOT/palindromeLL.java
+++ SDOT/palindromeLL.java
@@ -12,17 +12,65 @@ public class palindromeLL {
     public Node head;
     public Node tail;
 
+    public Node midNode(Node head){
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
     public boolean isPalindrome(Node head){
+        //0. base case
         if(head==null || head.next==null){
             return true;
         }
-        Node prev=null;
-        Node curr=head;
+        //1. find mid
+        Node midNode = midNode(head);
+        //2. reverse 2nd half
+        Node prev = null;
+        Node curr = midNode;
         Node next;
-
-        next=curr.next;
-        curr.next=prev;
-        prev=curr;
-        curr=next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        //3. compare
+        Node left = head;
+        Node right = prev;
+        while(right!=null){
+            if(left.data!=right.data){
+                return false;
+            }
+            left=left.next;
+            right=right.next;
+        }
+        return true;
+    }
+    public void creatLL(int data){
+        Node newNode = new Node(data);
+        if(head==null){
+            head=newNode;
+            tail=newNode;
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        palindromeLL LL1 = new palindromeLL();
+        for(int i=0;i<n;i++){
+            int data=sc.nextInt();
+            LL1.creatLL(data);
+        }
+        boolean isPalindrome = LL1.isPalindrome(LL1.head);
+        if(isPalindrome)
+            System.out.println("The linked list is a palindrome.");
+        else
+            System.out.println("The linked list is not a palindrome.");
     }
 }
